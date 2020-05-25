14db4f859bb884b341bed14517c98c884371f6b6
canikwe <c.anikwe@gmail.com>
2020-05-25 16:18:40 -0400

    solution

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,16 @@
 # Enter your procedural solution here!
+def sum_three_five_multiples(num)
+  (1...num).map do |x|
+    x % 3 == 0 || x % 5 == 0 ? x : 0
+  end.inject{|x, y| x + y}
+end
+
+def collect_multiples(num)
+  (1...num).select do |n|
+    n % 3 == 0 || n % 5 == 0
+  end
+end
+
+def sum_multiples(num)
+  collect_multiples(num).sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,19 @@
 # Enter your object-oriented solution here!
+
+class Multiples
+  attr_reader :limit
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+    (1...self.limit).select { |n| n % 3 == 0 || n % 5 == 0}
+  end
+
+  def sum_multiples
+    self.collect_multiples.sum
+  end
+
+
+end
